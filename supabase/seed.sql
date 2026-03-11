38ba8c520c0eadf184215a0f0083c9f410643eeb
James Mathew <mathewj2009@gmail.com>
2026-03-10 20:20:43 -0700

    adding admin user in progress

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,12 +1,37 @@
+WITH new_admin AS (
+INSERT INTO auth.users (
+    id,
+    email,
+    encrypted_password, 
+    email_confirmed_at,
+    created_at,
+    updated_at, 
+    aud, 
+    role
+)
+VALUES (
+    gen_random_uuid(), 
+    'admin@gmail.com',
+    crypt('Admin123!', gen_salt('bf')),
+    now(), 
+    now(),
+    now(),
+    'authenticated',
+    'authenticated'
+)
+RETURNING id, email
+)
 INSERT INTO public.users (
+    id,
     first_name,
     last_name,
     user_role,
     email
 )
-VALUES (
+SELECT
+    id,
     'Admin',
     'NSC',
     'admin',
     'admin@gmail.com'
-);
+FROM new_admin;
